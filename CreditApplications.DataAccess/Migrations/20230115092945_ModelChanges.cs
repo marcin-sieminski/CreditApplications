23ec0f84e2be41e2fc1969283e263b300ad846d9
Marcin Siemiński <marcin.sieminski@outlook.com>
2023-01-15 10:30:27 +0100

    Add model changes migration

---
+++ CreditApplications.DataAccess/Migrations/20230115092945_ModelChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreditApplications.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
