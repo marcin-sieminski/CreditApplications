e035c86db08cc32b7279f79bbeb0839d083bba6c
Marcin Siemiński <marcin.sieminski@outlook.com>
2023-01-22 12:32:39 +0100

    Add customer list view

--- ApplicationServices/Domain/Models/Customer.cs
+++ ApplicationServices/Domain/Models/Customer.cs
@@ -1,15 +1,30 @@
-﻿namespace CreditApplications.ApplicationServices.Domain.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreditApplications.ApplicationServices.Domain.Models;
 
 public class Customer
 {
+    [DisplayName("Id")]
     public int Id { get; set; }
+    [DisplayName("First name")]
+    [Required]
     public string CustomerFirstName { get; set; }
+    [DisplayName("Last name")]
+    [Required]
     public string CustomerLastName { get; set; }
+    [DisplayName("Country")]
     public string Country { get; set; }
+    [DisplayName("City")]
     public string City { get; set; }
+    [DisplayName("Postal code")]
     public string PostalCode { get; set; }
+    [DisplayName("Street")]
     public string Street { get; set; }
+    [DisplayName("House number")]
     public string AddressNumber { get; set; }
+    [DisplayName("Phone")]
     public string PhoneNumber { get; set; }
+    [DisplayName("E-mail")]
     public string Email { get; set; }
 }
--- CreditApplications.Web/Controllers/CustomersController.cs
+++ CreditApplications.Web/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using CreditApplications.ApplicationServices.Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using CreditApplications.ApplicationServices.Domain.Models;
 
 namespace CreditApplications.Web.Controllers
 {
@@ -8,9 +9,120 @@ namespace CreditApplications.Web.Controllers
         private readonly ILogger<CreditApplicationsController> _logger;
         private readonly ICustomerLogic _customersLogic;
 
-        public IActionResult List()
+        public CustomersController(ILogger<CreditApplicationsController> logger, ICustomerLogic customersLogic)
+        {
+            _logger = logger;
+            _customersLogic = customersLogic;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            try
+            {
+                var model = await _customersLogic.GetAll();
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to get customers: {e}");
+                return RedirectToAction(nameof(Error));
+            }
+        }
+
+        [Route("Details/{id:int}")]
+        public async Task<IActionResult> Details([FromRoute] int id)
+        {
+            try
+            {
+                var model = await _customersLogic.GetById(id);
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to get credit application details: {e}");
+                return RedirectToAction(nameof(Error));
+            }
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _customersLogic.Insert(model);
+                return RedirectToAction(nameof(List));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                _logger.LogInformation("Null ID passed to Edit route.");
+                return RedirectToAction(nameof(Error));
+            }
+
+            var model = await _customersLogic.GetById(id.Value);
+            if (model == null)
+            {
+                _logger.LogInformation("No product found for {id}.", id.Value);
+                return RedirectToAction(nameof(Error));
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Customer model)
+        {
+            if (id != model.Id)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _customersLogic.Update(model);
+                return RedirectToAction(nameof(List));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                var model = await _customersLogic.GetById(id.Value);
+                if (model == null)
+                {
+                    return RedirectToAction(nameof(Error));
+                }
+
+                return View(model);
+            }
+
+            return RedirectToAction(nameof(Error));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _customersLogic.Delete(id);
+            return RedirectToAction(nameof(List));
+        }
+
+        public IActionResult Error()
+        {
+            return View("NotFound");
         }
     }
 }
