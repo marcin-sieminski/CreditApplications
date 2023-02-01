513e378e13019e5cf447947f80ed1290c8266a63
Marcin Siemiński <marcin.sieminski@outlook.com>
2023-02-02 00:09:33 +0100

    Improve messages section

--- CreditApplications.DataAccess/Entities/Message.cs
+++ CreditApplications.DataAccess/Entities/Message.cs
@@ -1,9 +1,11 @@
-﻿namespace CreditApplications.DataAccess.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreditApplications.DataAccess.Entities;
 
 public class Message : EntityBase
 {
-    public string Title { get; set; }
-    public string Body { get; set; }
+    [Required] public string Title { get; set; }
+    [Required] public string Body { get; set; }
     public virtual List<Role> RolesToDistribute { get; set; }
 
 }
--- CreditApplications.Web/Controllers/MessageController.cs
+++ CreditApplications.Web/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
 ﻿using CreditApplications.DataAccess;
+using CreditApplications.DataAccess.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,11 +17,105 @@ public class MessageController : Controller
     public async Task<IActionResult> Index(int? id)
     {
         ViewBag.MessageModel = _context.Messages.Where(x => x.IsActive).OrderByDescending(x => x.Created).ToList();
-        var item = await _context.Messages.FirstOrDefaultAsync(x => x.IsActive && x.Id == id);
-        if (item == null)
+        var model = await _context.Messages.FirstOrDefaultAsync(x => x.IsActive && x.Id == id);
+        if (model == null)
         {
             return NotFound();
         }
-        return View(item);
+        return View(model);
+    }
+
+    public async Task<IActionResult> List()
+    {
+        var model = await _context.Messages.Where(x => x.IsActive).ToListAsync();
+        return View(model);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Message model)
+    {
+        if (ModelState.IsValid)
+        {
+            model.IsActive = true;
+            model.Created = DateTime.Now;
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+        return View(model);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return RedirectToAction(nameof(Error));
+        }
+
+        var model = await _context.Messages.SingleAsync(x => x.Id == id);
+        if (model == null)
+        {
+            return RedirectToAction(nameof(Error));
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Message model)
+    {
+        if (id != model.Id)
+        {
+            return RedirectToAction(nameof(Error));
+        }
+
+        if (ModelState.IsValid)
+        {
+            var dbEntity = _context.Messages.Single(x => x.Id == model.Id);
+            dbEntity.Body = model.Body;
+            dbEntity.Title = model.Title;
+            dbEntity.Modified = DateTime.Now;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+        return View(model);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id != null)
+        {
+            var model = _context.Messages.Single(x => x.Id == id);
+            if (model == null)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return View(model);
+        }
+
+        return RedirectToAction(nameof(Error));
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var dbEntity = _context.Messages.Single(x => x.Id == id);
+        dbEntity.IsActive = false;
+        dbEntity.Inactivated = DateTime.Now;
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(List));
+    }
+
+    public IActionResult Error()
+    {
+        return View("NotFound");
     }
 }
