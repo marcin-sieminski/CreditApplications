5291f6657cb05c4e185e84d88cfe519e0b2b02c7
Marcin Siemiński <marcin.sieminski@outlook.com>
2023-03-27 23:19:50 +0200

    Minor improvements

--- CreditApplications.Intranet/Helpers/Utilities.cs
+++ CreditApplications.Intranet/Helpers/Utilities.cs
@@ -4,12 +4,26 @@ namespace CreditApplications.Intranet.Helpers;
 
 public static class Utilities
 {
-    public static string IsActive(this IHtmlHelper html, string controller, string action)
+    public static string IsActive(this IHtmlHelper html, string controller, string action, int id)
+    {
+        var routeData = html.ViewContext.RouteData;
+        var routeAction = (string)routeData.Values["action"];
+        var routeController = (string)routeData.Values["controller"];
+        string routeId = null;
+        if (routeData.Values["id"] is not null)
+        {
+            routeId = routeData.Values["id"].ToString();
+        }
+        var returnActive = controller == routeController && action == routeAction && id.ToString() == routeId;
+        return returnActive ? "active bg-secondary text-white" : "";
+    }
+
+    public static string IsActiveTextSecondary(this IHtmlHelper html, string controller, string action)
     {
         var routeData = html.ViewContext.RouteData;
         var routeAction = (string)routeData.Values["action"];
         var routeControl = (string)routeData.Values["controller"];
         var returnActive = controller == routeControl && action == routeAction;
-        return returnActive ? "active" : "";
+        return returnActive ? "text-secondary" : "text-white";
     }
 }
